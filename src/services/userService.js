eb28cf48788a14a9c5f9a33a84f6aa03702133d7
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-25 22:34:09 -0300

    remove: arquivos de exeplos 'users'

--- src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import prisma from '../config/database.js';
-
-export const getAllUsers = async () => {
-  return await prisma.user.findMany();
-};
-
-export const createUser = async (userData) => {
-  return await prisma.user.create({ data: userData });
-};
