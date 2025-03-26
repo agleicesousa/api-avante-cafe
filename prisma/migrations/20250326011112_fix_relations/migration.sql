c0274963fc6b1284652a6ee3ef9dac64ed738ae7
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-25 22:11:35 -0300

    fix: relações das tabelas

--- prisma/migrations/20250326011112_fix_relations/migration.sql
+++ prisma/migrations/20250326011112_fix_relations/migration.sql
@@ -1,19 +1,17 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE "Mesa" (
+    "id" SERIAL NOT NULL,
+    "numero" INTEGER NOT NULL,
+    "ocupada" BOOLEAN NOT NULL DEFAULT false,
 
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
+    CONSTRAINT "Mesa_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Cliente" (
     "id" SERIAL NOT NULL,
     "nome" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "telefone" TEXT NOT NULL,
-    "endereco" TEXT NOT NULL,
+    "mesaId" INTEGER,
 
     CONSTRAINT "Cliente_pkey" PRIMARY KEY ("id")
 );
@@ -22,9 +20,9 @@ CREATE TABLE "Cliente" (
 CREATE TABLE "Pedido" (
     "id" SERIAL NOT NULL,
     "dataPedido" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "status" TEXT NOT NULL,
     "total" DECIMAL(65,30) NOT NULL,
     "clienteId" INTEGER NOT NULL,
+    "mesaId" INTEGER,
 
     CONSTRAINT "Pedido_pkey" PRIMARY KEY ("id")
 );
@@ -36,6 +34,8 @@ CREATE TABLE "Menu" (
     "descricao" TEXT NOT NULL,
     "preco" DECIMAL(65,30) NOT NULL,
     "categoria" TEXT NOT NULL,
+    "disponivel" BOOLEAN NOT NULL DEFAULT true,
+    "imagem" TEXT NOT NULL,
 
     CONSTRAINT "Menu_pkey" PRIMARY KEY ("id")
 );
@@ -47,15 +47,36 @@ CREATE TABLE "ItemPedido" (
     "precoUnitario" DECIMAL(65,30) NOT NULL,
     "pedidoId" INTEGER NOT NULL,
     "menuId" INTEGER NOT NULL,
+    "observacoes" TEXT,
 
     CONSTRAINT "ItemPedido_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Contato" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "mensagem" TEXT NOT NULL,
+    "dataMensagem" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Contato_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
-CREATE UNIQUE INDEX "Cliente_email_key" ON "Cliente"("email");
+CREATE UNIQUE INDEX "Mesa_numero_key" ON "Mesa"("numero");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contato_email_key" ON "Contato"("email");
+
+-- AddForeignKey
+ALTER TABLE "Cliente" ADD CONSTRAINT "Cliente_mesaId_fkey" FOREIGN KEY ("mesaId") REFERENCES "Mesa"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Pedido" ADD CONSTRAINT "Pedido_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "Cliente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Pedido" ADD CONSTRAINT "Pedido_mesaId_fkey" FOREIGN KEY ("mesaId") REFERENCES "Mesa"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ItemPedido" ADD CONSTRAINT "ItemPedido_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
