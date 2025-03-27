fbd41125de076aa4c4550e71d35bb6b06c95938d
Thales-hop <kauathales091@gmail.com>
2025-03-27 10:33:48 -0300

    adicionar rota GET para buscar contato por ID

--- routes/contatoRouter.js
+++ routes/contatoRouter.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const contatoController = require("../controllers/contatoController");
 
 router.post("/contatos", contatoController.criarContato);
+router.get("/contatos/:id", contatoController.buscarContatoPorId);
+
 
 module.exports = router;
