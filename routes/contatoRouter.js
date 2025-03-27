c5772c240155648d847da5b299b22f1c96ff52d4
Thales-hop <kauathales091@gmail.com>
2025-03-27 10:31:46 -0300

    adicionar rota GET para listar contatos

--- routes/contatoRouter.js
+++ routes/contatoRouter.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const contatoController = require("../controllers/contatoController");
 
+router.post("/contatos", contatoController.criarContato);
+
 module.exports = router;
