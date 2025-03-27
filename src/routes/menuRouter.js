761b63565021a4827dd483aa53d53f5fc2923811
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-27 06:47:24 -0300

    refactor: ajustar a formatação do menuRouter
    Co-authored-by: rafaelsr20 <rafaeldesr20@gmail.com>

--- src/routes/menuRouter.js
+++ src/routes/menuRouter.js
@@ -1,19 +1,16 @@
 import express from 'express';
-import { 
-  criarMenu, 
-  listarItens, 
-  buscaDeCategoria, 
+import {
+  criarMenu,
+  listarItens,
+  buscaDeCategoria,
   listagemTotal
 } from '../controllers/menuController.js';
 
 const router = express.Router();
 
 router.post('/menu', criarMenu);
-
 router.get('/menu/:id', listarItens);
-
 router.get('/menu/categoria/:categoria', buscaDeCategoria);
-
 router.get('/menu', listagemTotal);
 
 export default router;
